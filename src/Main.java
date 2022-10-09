84c90a7487c1b64073aa11cd9e9c8196eb171dde
Wendel <wendelljuliao@hotmail.com>
2022-10-09 09:50:06 -0300

    ajustes no código e parar o while so quando achar solucao otima

--- src/Main.java
+++ src/Main.java
@@ -18,22 +18,6 @@ public class Main {
 				quantAtaquesNaPosicao++;
 			}
 		}
-//		for (int i = 0; i < matriz.length; i++) {
-//			for (int j = 0; j < matriz[i].length; j++) {
-//
-//// verificando na linha ou na coluna se tem sob ataque
-//				if (i != linha || j != coluna) {
-//
-//					if (i == linha && matriz[i][j] == 1) {
-//						return true;
-//					}
-//
-//					if (j == coluna && matriz[i][j] == 1) {
-//						return true;
-//					}
-//				}
-//			}
-//		}
 
 		int linhaAux = linha + 1;
 		int colunaAux = coluna + 1;
@@ -85,11 +69,19 @@ public class Main {
 	public static void main(String[] args) {
 		Main rainhas = new Main();
 		int[][] matriz = new int[8][8];
+		ArrayList<int[]> sobAtaqueEmpatados = new ArrayList<>();
 
 		final int QUANTIDADE_RAINHAS = 8;
 
 		int quantRainhasColocados = 0;
+		int quantAtaquesAtual = 0;
+		int menorQuantAtaquesAnterior;
 
+		int[] vetorPosicaoEscolhida = new int[2];
+		
+		int quantVezesAteSolucao = 0;
+
+		// Criação do tabuleiro
 		for (int i = 0; i < matriz.length; i++) {
 			for (int j = 0; j < matriz[i].length; j++) {
 				System.out.print(matriz[i][j] + " ");
@@ -99,61 +91,71 @@ public class Main {
 
 		System.out.println();
 
-		int menorQuantAtaquesAnterior;
+		while (quantRainhasColocados < 8) {
+			quantRainhasColocados = 0;
+			matriz = new int[8][8];
 
-		int quantAtaquesAtual = 0;
-		
-		ArrayList<int[]> sobAtaqueEmpatados = new ArrayList<>(); 
-		
-		int[] vetorPosicaoEscolhida = new int[2];
-		
-		for (int i = 0; i < QUANTIDADE_RAINHAS; i++) {
-			menorQuantAtaquesAnterior = Integer.MAX_VALUE;
-			sobAtaqueEmpatados.clear();
+			for (int i = 0; i < QUANTIDADE_RAINHAS; i++) {
+				menorQuantAtaquesAnterior = Integer.MAX_VALUE;
+				sobAtaqueEmpatados.clear();
 
-			// descobrir qual é a menor quantAtaques em uma posição 
-			for (int k = 0; k < matriz.length; k++) {
-				for (int j = 0; j < matriz[k].length; j++) {
-					quantAtaquesAtual = rainhas.sobAtaque(matriz, k, j);
-					if (quantAtaquesAtual < menorQuantAtaquesAnterior && matriz[k][j] != 1) {
-						menorQuantAtaquesAnterior = quantAtaquesAtual;
+				// descobrir qual é a menor quantAtaques em uma posição
+				for (int k = 0; k < matriz.length; k++) {
+					for (int j = 0; j < matriz[k].length; j++) {
+						quantAtaquesAtual = rainhas.sobAtaque(matriz, k, j);
+						if (quantAtaquesAtual < menorQuantAtaquesAnterior && matriz[k][j] != 1) {
+							menorQuantAtaquesAnterior = quantAtaquesAtual;
+						}
 					}
 				}
-			}	
-			
-			// verificar se tem mais de uma menor em quantAtaques em várias posições
-			for (int k = 0; k < matriz.length; k++) {
-				for (int j = 0; j < matriz[k].length; j++) {
-					quantAtaquesAtual = rainhas.sobAtaque(matriz, k, j);
-					if (quantAtaquesAtual == menorQuantAtaquesAnterior && matriz[k][j] != 1) {
-						int[] vetor = new int[2];
-						vetor[0] = k;
-						vetor[1] = j;
-						sobAtaqueEmpatados.add(vetor);						
+
+				// verificar se tem mais de uma menor em quantAtaques em várias posições
+				for (int k = 0; k < matriz.length; k++) {
+					for (int j = 0; j < matriz[k].length; j++) {
+						quantAtaquesAtual = rainhas.sobAtaque(matriz, k, j);
+						if (quantAtaquesAtual == menorQuantAtaquesAnterior && matriz[k][j] != 1) {
+							int[] vetor = new int[2];
+							vetor[0] = k;
+							vetor[1] = j;
+							sobAtaqueEmpatados.add(vetor);
+						}
 					}
 				}
-			}
-			
-			if (menorQuantAtaquesAnterior == Integer.MAX_VALUE || menorQuantAtaquesAnterior > 0) {
-				break;
-			}
-						
-			vetorPosicaoEscolhida = sobAtaqueEmpatados.get(new Random().nextInt(sobAtaqueEmpatados.size()));
+
+				if (menorQuantAtaquesAnterior == Integer.MAX_VALUE || menorQuantAtaquesAnterior > 0) {
+					break;
+				}
+
+				vetorPosicaoEscolhida = sobAtaqueEmpatados.get(new Random().nextInt(sobAtaqueEmpatados.size()));
 //			System.out.println(vetorPosicaoEscolhida[0] + " " + vetorPosicaoEscolhida[1]);
-			
-			matriz[vetorPosicaoEscolhida[0]][vetorPosicaoEscolhida[1]] = 1;
-			quantRainhasColocados++;				
-			
-		}
 
-		for (int i = 0; i < matriz.length; i++) {
-			for (int j = 0; j < matriz[i].length; j++) {
-				System.out.print(matriz[i][j] + " ");
+				matriz[vetorPosicaoEscolhida[0]][vetorPosicaoEscolhida[1]] = 1;
+				quantRainhasColocados++;
+
 			}
+			for (int i = 0; i < matriz.length; i++) {
+				for (int j = 0; j < matriz[i].length; j++) {
+					System.out.print(matriz[i][j] + " ");
+				}
+				System.out.println();
+			}
+
+			System.out.println("Quantidade de rainhas no mapa: " + quantRainhasColocados);
 			System.out.println();
+			
+			quantVezesAteSolucao++;
 		}
 		
-		System.out.println("Quantidade de rainhas no mapa: " + quantRainhasColocados);
+		System.out.println("Precisou de: " + quantVezesAteSolucao + " jogadas para achar a solução");
+
+//		for (int i = 0; i < matriz.length; i++) {
+//			for (int j = 0; j < matriz[i].length; j++) {
+//				System.out.print(matriz[i][j] + " ");
+//			}
+//			System.out.println();
+//		}
+//
+//		System.out.println("Quantidade de rainhas no mapa: " + quantRainhasColocados);
 
 	}
 
